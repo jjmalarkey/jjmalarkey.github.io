156983342029d9d91fa55a6d147519fc92ad0dec
jjmalarkey <38889395+jjmalarkey@users.noreply.github.com>
2019-04-14 20:27:12 -0700

    foundation for aoe preview

--- src/controller.js
+++ src/controller.js
@@ -1,5 +1,56 @@
 var app = angular.module('myApp', []);
 app.controller('testCtrl', function($scope) {
-	$scope.firstName = "Hello";
-	$scope.lastName = "there";
+	$scope.gridWidth = 450;
+	$scope.gridHeight = 450;
+
+	$scope.areaType = "";
+	$scope.areaTypeShapes = ['Line', 'Sphere', 'Cube'];
+	$scope.spellRange;
+	$scope.spellWidth;
+	$scope.spellRadius;
+
+	$scope.drawing = function() {
+		var canvas = document.getElementById('tutorial');
+		var ctx = canvas.getContext('2d');
+		ctx.canvas.height = $scope.gridHeight;
+		ctx.canvas.width = $scope.gridWidth;
+		var gridsizepx = 50;
+		console.log('range', $scope.spellRange);
+
+		for(var i = 0; i < $scope.gridWidth; i = i + gridsizepx) {
+			ctx.beginPath();
+			ctx.lineTo(i, 0);
+			ctx.lineTo(i, $scope.gridHeight);
+			ctx.stroke();
+		}
+
+		for(var i = 0; i < $scope.gridHeight; i = i + gridsizepx) {
+			ctx.beginPath();
+			ctx.lineTo(0, i);
+			ctx.lineTo($scope.gridHeight, i);
+			ctx.stroke();
+		}
+
+		if($scope.areaType == 'Line') {
+			ctx.beginPath();
+			ctx.arc(25,225,20,0,2*Math.PI);
+			ctx.fill();
+
+			ctx.strokeStyle = 'red';
+			ctx.beginPath();
+			ctx.moveTo(25,225);
+			ctx.lineTo(($scope.spellRange*10)+gridsizepx, 225);
+			ctx.stroke();
+		}
+		else if($scope.areaType =='Cube') {
+		/*	ctx.strokeStyle = 'red';
+			ctx.beginPath();
+			ctx.arc(25,225,10,0,2*Math.PI);
+			ctx.fill();
+*/
+			ctx.fillStyle = 'red';
+			ctx.globalAlpha = 0.5;
+			ctx.fillRect(100,100,200,200);
+		}
+	}
 });
